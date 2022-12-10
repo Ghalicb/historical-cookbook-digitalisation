849ed65c0bfbc2c0914566404a1dc299f77d8eef
Ghali C <ghalicb@gmail.com>
2022-12-10 16:42:45 +0100

    Add constant file with Units set & Region2SubRegion map

---
+++ src/constant.py
@@ -0,0 +1,18 @@
+
+
+
+Units = {
+	'litre', 'litres', 'l', 'cl', 'dl', 'kg', 'g', 'pincée', 'cuil.', 'cuil. café', 'cuil. soupe', 'cuil. à soupe', 
+	'petite cuil.', 'grande cuil.', 'verre', 'verres', 'petit verre', 'verre à liqueur', 'verres à liqueur', 'tasse', 
+	'tasses', 'bout.', 'bouteille', 'bouteilles', 'grande boîte', 'gousse', 'gousses', 'branche', 'branches', 'membre', 
+	'membres', 'tronçon', 'tronçons', 'tranche', 'tranches', 'tube', 'tubes'
+}
+
+Region2SubRegion = {	
+	"Paris, Ile de France, Val de Loire": ['Paris', 'Ile-de-France', 'Orléans', 'Touraine'],
+	"Pays de l’ouest": ['Anjou', 'Bretagne', 'Poitou Vendée', 'Charentes'],
+	"Sud-ouest & pyrénées": ['Bordelais', 'Gascogne', 'Pays Basque', 'Roussillon', 'Périgord', 'Languedoc'], 
+	"Sud-est & méditérannée": ['Provence', 'Nice', 'Corse', 'Dauphiné', 'Savoie', 'Lyon', 'Auvergne', 'Limousin'],
+	"Bourgogne, Champagne, Bresse, Franche Comte, Alsace, Lorraine": ['Bourgogne', 'Champagne', 'Bresse', 'Franche-Compté', 'Alsace', 'Lorraine'],
+	"Nord & Normandie": ['Nord', 'Normandie']
+}
