06623210bc37092bb112657688e416886144963f
Ghali C <ghalicb@gmail.com>
2022-12-20 19:11:03 +0100

    Add first constant for categories

--- src/constant.py
+++ src/constant.py
@@ -1,4 +1,21 @@
 
+Categories = {
+	'Viande': ['viande', 'oie', 'canard', 'oiseau', 'lard', 'bœuf', 'veau', 'poule', 'poulet', 'poularde', 'volaille', 'porc', 'caille', 'canard', 'caneton', 'mouton', 'cochon', 'coq', 'chevreuil', 'lièvre', 'levraut', 'lapin', 'faisan', 'gibier', 'jambon', 'chorizo', 'cervelas', 'agneau', 'escargot', 'grenouille'],
+	'Poisson': ['poisson', 'brochet', 'carpe', 'morue', 'lamproie', 'lotte', 'maquereau', 'omble', 'rouget', 'sardine', 'thon', 'truite', 'anchois', 'anguille', 'merlan', 'sole', 'barbue', 'turbot', 'raie', 'perche', 'saumon', 'colin', 'goujon', 'loup', 'congre', 'rascasse', 'grondin', 'merlu', 'merluza', 'hareng', 'alose', 'brême'],
+	"Fruit de mer": ['crevette', 'langouste', 'moule', 'écrevisse', 'palourde', 'homard', 'chiperon', 'seiche', 'huître', 'coquille', 'poulpe'],
+	'Alcool': ['alcool', 'bière', 'vin', 'cidre', 'fine', 'liqueur'],
+	"Plante aromatique": ["bouquet garni", 'ail', 'anis', 'aromate', 'angélique', 'basilic', 'persil', 'sarriette', 'cerfeuil', 'ciboule', 'ciboulette', "clou de girofle", "clous de girofle", 'girofle', 'cive', 'câpre', 'estragon', "feuille de vigne", "fines herbes", 'laurier', 'menthe', 'pissenlit', 'romarin', 'thym'],
+	'Epice': ['cannelle', 'coriandre', 'curry', 'safran', 'poivre', 'sel', 'moutarde', 'muscade', 'paprika', 'piment', 'sauge', 'serpolet', 'épices'],
+	"Produit laitier": ['lait', 'crème', 'fromage', 'gruyère', 'parmesan'],
+	'Légume': ['artichaut', 'asperge', 'aubergine', 'bette', 'betterave', 'cardon', 'chou', 'cornichon', 'courgette', 'cresson', 'céleri', 'fenouil', 'légume', 'navet', 'panais', 'poireau', "pomme de terre", "pommes de terre", 'potiron', 'rave', 'salade', 'tomate', 'échalote', 'épinard'],
+	'Fruit': ['abricot', 'banane', 'cerise', 'coing', 'fraise', 'framboise', 'groseille', 'raisin', 'olive', 'orange', 'pomme'],
+	'Agrume': ['citron', 'cédrat', "fleur d'oranger", "fleurs d'oranger"],
+	'Céréale': ['farine', 'pain', 'pâte', 'riz'],
+	'Légumineuse': ['févette', 'haricot', 'pois'],
+	"Fruit sec": ['amande', 'noix', 'noisette'],
+	'Champignon': ['champignon', 'truffe', 'cèpe', 'girofle', 'morille', 'levure', 'oronge', 'duelle']
+}
+
 Units = [
 	# Spoons
 	'cuil. à café', 'cuil. café', 'cuil. à soupe', 'cuil. soupe', 'petite cuil.', 'grande cuil.', 'cuil.', 
@@ -17,10 +34,10 @@ Units = [
 ]
 
 Region2SubRegion = {
-	"Paris, Ile de France, Val de Loire": ['Paris', 'Ile-de-France', 'Orléans', 'Touraine'],
-	"Pays de l’ouest": ['Anjou', 'Bretagne', 'Poitou Vendée', 'Charentes'],
-	"Sud-ouest & pyrénées": ['Bordelais', 'Gascogne', 'Pays Basque', 'Roussillon', 'Périgord', 'Languedoc'], 
-	"Sud-est & méditérannée": ['Provence', 'Nice', 'Corse', 'Dauphiné', 'Savoie', 'Lyon', 'Auvergne', 'Limousin'],
-	"Bourgogne, Champagne, Bresse, Franche Comte, Alsace, Lorraine": ['Bourgogne', 'Champagne', 'Bresse', 'Franche-Compté', 'Alsace', 'Lorraine'],
+	"Paris, Ile-de-France, Val de Loire": ['Paris', 'Ile-de-France', 'Orléans', 'Touraine'],
+	"Pays de l’Ouest": ['Anjou', 'Bretagne', 'Poitou Vendée', 'Charentes'],
+	"Sud-Ouest & Pyrénées": ['Bordelais', 'Gascogne', 'Pays Basque', 'Roussillon', 'Périgord', 'Languedoc'], 
+	"Sud-Est & Méditérannée": ['Provence', 'Nice', 'Corse', 'Dauphiné', 'Savoie', 'Lyon', 'Auvergne', 'Limousin'],
+	"Bourgogne, Champagne, Bresse, Franche-Comté, Alsace, Lorraine": ['Bourgogne', 'Champagne', 'Bresse', 'Franche-Comté', 'Alsace', 'Lorraine'],
 	"Nord & Normandie": ['Nord', 'Normandie']
 }
