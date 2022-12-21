ca51a605833ac80f769aa20b65cc9c84ba3e8d51
Ghali C <ghalicb@gmail.com>
2022-12-21 13:45:44 +0100

    Add plurals to categories & clean constants

--- src/constant.py
+++ src/constant.py
@@ -1,19 +1,75 @@
 
 Categories = {
-	'Viande': ['viande', 'oie', 'canard', 'oiseau', 'lard', 'bœuf', 'veau', 'poule', 'poulet', 'poularde', 'volaille', 'porc', 'caille', 'canard', 'caneton', 'mouton', 'cochon', 'coq', 'chevreuil', 'lièvre', 'levraut', 'lapin', 'faisan', 'gibier', 'jambon', 'chorizo', 'cervelas', 'agneau', 'escargot', 'grenouille'],
-	'Poisson': ['poisson', 'brochet', 'carpe', 'morue', 'lamproie', 'lotte', 'maquereau', 'omble', 'rouget', 'sardine', 'thon', 'truite', 'anchois', 'anguille', 'merlan', 'sole', 'barbue', 'turbot', 'raie', 'perche', 'saumon', 'colin', 'goujon', 'loup', 'congre', 'rascasse', 'grondin', 'merlu', 'merluza', 'hareng', 'alose', 'brême'],
-	"Fruit de mer": ['crevette', 'langouste', 'moule', 'écrevisse', 'palourde', 'homard', 'chiperon', 'seiche', 'huître', 'coquille', 'poulpe'],
-	'Alcool': ['alcool', 'bière', 'vin', 'cidre', 'fine', 'liqueur'],
-	"Plante aromatique": ["bouquet garni", 'ail', 'anis', 'aromate', 'angélique', 'basilic', 'persil', 'sarriette', 'cerfeuil', 'ciboule', 'ciboulette', "clou de girofle", "clous de girofle", 'girofle', 'cive', 'câpre', 'estragon', "feuille de vigne", "fines herbes", 'laurier', 'menthe', 'pissenlit', 'romarin', 'thym'],
-	'Epice': ['cannelle', 'coriandre', 'curry', 'safran', 'poivre', 'sel', 'moutarde', 'muscade', 'paprika', 'piment', 'sauge', 'serpolet', 'épices'],
-	"Produit laitier": ['lait', 'crème', 'fromage', 'gruyère', 'parmesan'],
-	'Légume': ['artichaut', 'asperge', 'aubergine', 'bette', 'betterave', 'cardon', 'chou', 'cornichon', 'courgette', 'cresson', 'céleri', 'fenouil', 'légume', 'navet', 'panais', 'poireau', "pomme de terre", "pommes de terre", 'potiron', 'rave', 'salade', 'tomate', 'échalote', 'épinard'],
-	'Fruit': ['abricot', 'banane', 'cerise', 'coing', 'fraise', 'framboise', 'groseille', 'raisin', 'olive', 'orange', 'pomme'],
-	'Agrume': ['citron', 'cédrat', "fleur d'oranger", "fleurs d'oranger"],
-	'Céréale': ['farine', 'pain', 'pâte', 'riz'],
-	'Légumineuse': ['févette', 'haricot', 'pois'],
-	"Fruit sec": ['amande', 'noix', 'noisette'],
-	'Champignon': ['champignon', 'truffe', 'cèpe', 'girofle', 'morille', 'levure', 'oronge', 'duelle']
+	'Viande': [
+		'agneau', 'agneaux', 'bœuf', 'bœufs', 'caille', 'cailles', 'canard', 'canards', 'caneton', 'canetons',
+		'cervelas', 'chevreuil', 'chevreuils', 'chorizo', 'chorizos', 'cochon', 'cochons', 'coq', 'coqs',
+		'escargot', 'escargots', 'faisan', 'faisans', 'gibier', 'gibiers', 'grenouille', 'grenouilles',
+		'jambon', 'jambons', 'lapin', 'lapins', 'lard', 'lards', 'levraut', 'levrauts', 'lièvre', 'lièvres',
+		'mouton', 'moutons', 'oie', 'oies', 'oiseau', 'oiseaux', 'porc', 'porcs', 'poularde', 'poulardes',
+		'poule', 'poules', 'poulet', 'poulets', 'veau', 'veaux', 'viande', 'viandes', 'volaille', 'volailles'
+	],
+	'Poisson': [
+		'alose', 'aloses', 'anchois', 'anguille', 'anguilles', 'barbue', 'barbues', 'brochet', 'brochets',
+		'brême', 'brêmes', 'carpe', 'carpes', 'colin', 'colins', 'congre', 'congres', 'goujon', 'goujons',
+		'grondin', 'grondins', 'hareng', 'harengs', 'lamproie', 'lamproies', 'lotte', 'lottes', 'loup', 
+		'loups', 'maquereau', 'maquereaux', 'merlan', 'merlans', 'merlu', 'merlus', 'merluza', 'merluzas',
+		'morue', 'morues', 'omble', 'ombles', 'perche', 'perches', 'poisson', 'poissons', 'raie', 'raies',
+		'rascasse', 'rascasses', 'rouget', 'rougets', 'sardine', 'sardines', 'saumon', 'saumons', 'sole',
+		'soles', 'thon', 'thons', 'truite', 'truites', 'turbot', 'turbots'
+	],
+	"Fruit de mer": [
+		'chiperon', 'chiperons', 'coquille', 'coquilles', 'crevette', 'crevettes', 'homard', 'homards',
+		'huître', 'huîtres', 'langouste', 'langoustes', 'moule', 'moules', 'palourde', 'palourdes',
+		'poulpe', 'poulpes', 'seiche', 'seiches', 'écrevisse', 'écrevisses'
+	],
+	'Alcool': [
+		# No plural necessary for alcool
+		'alcool', 'bière', 'cidre', 'fine', 'liqueur', 'vin'
+	],
+	"Plante aromatique": [
+		# With plural form when countable
+		'ail', 'angélique', 'angéliques', 'anis', 'aromate', 'aromates', 'basilic', 'bouquet garni',
+		'bouquets garnis', 'cerfeuil', 'ciboule', 'ciboules', 'ciboulette', 'ciboulettes', 'cive', 'cives',
+		'clou de girofle', 'clous de girofle', 'câpre', 'câpres', 'estragon', 'feuille de vigne', 
+		'feuilles de vigne', 'fines herbes', 'girofle', 'laurier', 'menthe', 'persil', 'pissenlit', 'romarin',
+		'sarriette', 'thym'
+	],
+	'Epice': [
+		# With plural form when countable
+		'cannelle', 'coriandre', 'curry', 'moutarde', 'muscade', 'paprika', 'piment', 'piments', 'poivre',
+		'safran', 'sauge', 'sel', 'serpolet', 'épices'
+	],
+	"Produit laitier": [
+		# No plural necessary for dairy product
+		'crème', 'fromage', 'gruyère', 'lait', 'parmesan'
+	],
+	'Légume': [
+		'artichaut', 'artichauts', 'asperge', 'asperges', 'aubergine', 'aubergines', 'bette', 'bettes',
+		'betterave', 'betteraves', 'cardon', 'cardons', 'chou', 'choux', 'cornichon', 'cornichons',
+		'courgette', 'courgettes', 'cresson', 'céleri', 'fenouil', 'fenouils', 'légume', 'légumes',
+		'navet', 'navets', 'panais', 'poireau', 'poireaux', 'pomme de terre', 'pommes de terre', 'potiron', 
+		'potirons', 'rave', 'raves', 'salade', 'tomate', 'tomates', 'échalote', 'échalotes', 'épinard', 
+		'épinards'
+	],
+	'Fruit': [
+		'abricot', 'abricots', 'banane', 'bananes', 'cerise', 'cerises', 'cédrat', 'cédrats', 'citron', 
+		'citrons', 'coing', 'coings', "fleur d'oranger", "fleurs d'oranger", 'fraise', 'fraises',
+		'framboise', 'framboises', 'groseille', 'groseilles', 'olive', 'olives', 'orange', 'oranges'
+		'pomme', 'pommes', 'raisin', 'raisins'
+	],
+	'Céréale': [
+		'farine', 'pain', 'pains', 'pâte', 'riz'
+	],
+	'Légumineuse': [
+		'févette', 'févettes', 'haricot', 'haricots', 'pois'
+	],
+	"Fruit sec": [
+		'amande', 'amandes', 'noisette', 'noisettes', 'noix'
+	],
+	'Champignon': [
+		'champignon', 'champignons', 'cèpe', 'cèpes', 'duxelle', 'girofle', 'girofles', 'levure', 'morille',
+		'morilles', 'oronge', 'oronges', 'truffe', 'truffes'
+	]
 }
 
 Units = [
@@ -34,10 +90,22 @@ Units = [
 ]
 
 Region2SubRegion = {
-	"Paris, Ile-de-France, Val de Loire": ['Paris', 'Ile-de-France', 'Orléans', 'Touraine'],
-	"Pays de l’Ouest": ['Anjou', 'Bretagne', 'Poitou Vendée', 'Charentes'],
-	"Sud-Ouest & Pyrénées": ['Bordelais', 'Gascogne', 'Pays Basque', 'Roussillon', 'Périgord', 'Languedoc'], 
-	"Sud-Est & Méditérannée": ['Provence', 'Nice', 'Corse', 'Dauphiné', 'Savoie', 'Lyon', 'Auvergne', 'Limousin'],
-	"Bourgogne, Champagne, Bresse, Franche-Comté, Alsace, Lorraine": ['Bourgogne', 'Champagne', 'Bresse', 'Franche-Comté', 'Alsace', 'Lorraine'],
-	"Nord & Normandie": ['Nord', 'Normandie']
+	"Paris, Ile-de-France, Val de Loire": [
+		'Paris', 'Ile-de-France', 'Orléans', 'Touraine'
+	],
+	"Pays de l’Ouest": [
+		'Anjou', 'Bretagne', 'Poitou Vendée', 'Charentes'
+	],
+	"Sud-Ouest & Pyrénées": [
+		'Bordelais', 'Gascogne', 'Pays Basque', 'Roussillon', 'Périgord', 'Languedoc'
+	], 
+	"Sud-Est & Méditérannée": [
+		'Provence', 'Nice', 'Corse', 'Dauphiné', 'Savoie', 'Lyon', 'Auvergne', 'Limousin'
+	],
+	"Bourgogne, Champagne, Bresse, Franche-Comté, Alsace, Lorraine": [
+		'Bourgogne', 'Champagne', 'Bresse', 'Franche-Comté', 'Alsace', 'Lorraine'
+	],
+	"Nord & Normandie": [
+		'Nord', 'Normandie'
+	]
 }
